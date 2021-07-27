3d8e3d8dcca992f4854db2613347437babeb879a
Taylor Downs <taylor@openfn.org>
2021-07-27 22:28:50 +0100

    taylor@openfn.org's change on OpenFn.org

--- sf.js
+++ sf.js
@@ -1,1 +1,13 @@
-// Your job goes here.
+upsert('Contact', 'Contact_id__c', fields(
+  field('Name', dataValue('contactPerson')),
+  field('MailingAddress', dataValue('Address')),
+  field('Email', dataValue('email')),
+  field('MobilePhone', dataValue('phone')),
+));
+
+upsert('Patient_Satisfaction_Survey__c', 'Survey_id__c', fields(
+  field('Region__c', dataValue('Area')),
+  field('Facility_Ownership__c', dataValue('Facility Ownership')),
+  field('Facility_Type__c', dataValue('Facility Type')),
+  field('Location__c', dataValue('Location_Rural/Urban')),
+));

201c53b4cfdd623a7181668450d4ff353d48a93e
Taylor Downs <downs.taylor@gmail.com>
2021-08-04 14:44:34 +0100

    only use dhis2 teis with names and IDs

--- dhis2.js
+++ dhis2.js
@@ -13,12 +13,14 @@ fn(state => {
   const { trackedEntityInstances } = state.data;
   console.log('Sample TEI:', trackedEntityInstances[0]);
 
-  const preparedData = trackedEntityInstances.map(tei => ({
-    firstName: tei.attributes.find(x => x.displayName === 'First name'),
-    lastName: tei.attributes.find(x => x.displayName === 'Last name'),
-    id: tei.attributes.find(x => x.code === 'MMD_PER_ID'),
-    ...tei,
-  }));
+  const preparedData = trackedEntityInstances
+    .map(tei => ({
+      firstName: tei.attributes.find(x => x.displayName === 'First name'),
+      lastName: tei.attributes.find(x => x.displayName === 'Last name'),
+      id: tei.attributes.find(x => x.code === 'MMD_PER_ID'),
+      ...tei,
+    }))
+    .filter(tei => tei.firstName && tei.lastName && tei.id);
 
   // return { ...state, data: preparedData.shift() };
   return { ...state, data: preparedData };

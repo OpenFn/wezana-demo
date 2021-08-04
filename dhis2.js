95a402c18162e034a67361b8a3366f212d5e3643
Taylor Downs <downs.taylor@gmail.com>
2021-08-04 14:40:08 +0100

    setup for explanation

--- dhis2.js
+++ dhis2.js
@@ -1,6 +1,20 @@
 getTEIs({
   fields: '*',
-  ou: 'CMqUILyVnBL',
-  trackedEntityInstance: 'HNTA9qD6EEG',
+  ou: 'DiszpKrYNg8',
   skipPaging: true,
+});
+
+fn(state => {
+  const { trackedEntityInstances } = state.data;
+  console.log('Sample TEI:', trackedEntityInstances[0]);
+
+  const preparedData = trackedEntityInstances.map(tei => ({
+    firstName: tei.attributes.find(x => x.displayName === 'First name'),
+    lastName: tei.attributes.find(x => x.displayName === 'Last name'),
+    id: tei.attributes.find(x => x.code === 'MMD_PER_ID'),
+    ...tei,
+  }));
+
+  // return { ...state, data: preparedData.shift() };
+  return { ...state, data: preparedData };
 });
--- sf.js
+++ sf.js
@@ -1,13 +1,52 @@
-upsert('Contact', 'Contact_id__c', fields(
-  field('Name', dataValue('contactPerson')),
-  field('MailingAddress', dataValue('Address')),
-  field('Email', dataValue('email')),
-  field('MobilePhone', dataValue('phone')),
-));
+// for a single one...
+// upsert(
+//   'Contact',
+//   'DHIS2_ID__c',
+//   fields(
+//     field('DHIS2_ID__c', dataValue('id.value')),
+//     field('FirstName', dataValue('firstName.value')),
+//     field('LastName', dataValue('lastName.value')),
+//     field('MailingAddress', dataValue('Address')),
+//     field('Email', dataValue('email')),
+//     field('MobilePhone', dataValue('phone'))
+//   )
+// );
 
-upsert('Patient_Satisfaction_Survey__c', 'Survey_id__c', fields(
-  field('Region__c', dataValue('Area')),
-  field('Facility_Ownership__c', dataValue('Facility Ownership')),
-  field('Facility_Type__c', dataValue('Facility Type')),
-  field('Location__c', dataValue('Location_Rural/Urban')),
-));
+// upsert(
+//   'Patient_Satisfaction_Survey__c',
+//   'Survey_id__c',
+//   fields(
+//     field('Region__c', dataValue('Area')),
+//     field('Facility_Ownership__c', dataValue('Facility Ownership')),
+//     field('Facility_Type__c', dataValue('Facility Type')),
+//     field('Location__c', dataValue('Location_Rural/Urban'))
+//   )
+// );
+
+// or for bulk...
+bulk(
+  'Contact',
+  'upsert',
+  { extIdField: 'DHIS2_ID__c', failOnError: true },
+  state =>
+    state.data.map(tei => {
+      return {
+        DHIS2_ID__c: tei.id.value,
+        FirstName: tei.firstName.value,
+        LastName: tei.lastName.value,
+        MailingAddress: tei.Address,
+        Email: tei.email,
+        MobilePhone: tei.phone,
+      };
+    })
+);
+
+bulk('Patient_Satisfaction_S__c', 'insert', { failOnError: true }, state =>
+  state.data.map(tei => ({
+    'Contact__r.DHIS2_ID__c': tei.id.value,
+    Region__c: 'West',
+    Facility_Ownership__c: 'NGO',
+    Facility_Type__c: 'Hospital',
+    Location__c: 'Rural',
+  }))
+);

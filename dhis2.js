e45491a35ec1771d89ab1050e354b78c908cad58
Taylor Downs <downs.taylor@gmail.com>
2021-08-04 15:05:57 +0100

    no duplicates from dhis2

--- dhis2.js
+++ dhis2.js
@@ -19,7 +19,11 @@ fn(state => {
       id: tei.attributes.find(x => x.code === 'MMD_PER_ID'),
       ...tei,
     }))
-    .filter(tei => tei.firstName && tei.lastName && tei.id);
+    .filter(tei => tei.firstName && tei.lastName && tei.id)
+    .filter((elem, i, arr) => {
+      const first = arr.find(x => x.id.value === elem.id.value);
+      return i === arr.indexOf(first);
+    });
 
   console.log('Sample TEI:', preparedData[0]);
   // return { ...state, data: preparedData.shift() };
